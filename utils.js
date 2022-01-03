70442f9e4ab1da3b2f43adf6d8a2df22dd549bd0
rseusebio <rafaelsantos.rse@gmail.com>
2022-01-03 17:04:03 -0300

    added cookies funcionality

--- utils.js
+++ utils.js
@@ -2,9 +2,7 @@ function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
-    console.log("to save", document);
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    console.log("saved", document.cookie);
 }
 
 function getCookie(cname) {
