f04ff4d9175b366935817439776b747881011f2a
rseusebio <rafaelsantos.rse@gmail.com>
2022-01-03 16:55:18 -0300

    cookies

--- utils.js
+++ utils.js
@@ -1,25 +1,24 @@
 function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays*24*60*60*1000));
-  let expires = "expires="+ d.toUTCString();
-  console.log("to save", document);
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  console.log("saved", document.cookie);
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    console.log("to save", document);
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    console.log("saved", document.cookie);
 }
 
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
     return "";
-  }
-  
+}
